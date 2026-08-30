84851e63182336590ac4e4b54861bde4cded0e85
jammar24 <jamarodalis@yahoo.com>
2026-08-29 22:11:50 -0500

    Returning strings and message  practice and excercise

---
+++ ReturningStrings.java
@@ -0,0 +1,17 @@
+/*
+
+*/
+
+
+public class Kata
+{
+    public static String greet(String name)
+    {
+        return "Hello, " + name + " how are you doing today?"; // esta parte  retorna el mensaje y nombre
+    }
+
+    public static void main(String[] args) {
+        String message = greet("Ryan"); // aqui estamos llamando la funcion y , dentro de una nueva variable que renombramos como  mensaje
+        System.out.println(message);
+    }
+}// escribire el codigo en ingles para practicar y en español mis comentarios para ya no  perder el hilo de lo que pensaba cuando escribi esto 
